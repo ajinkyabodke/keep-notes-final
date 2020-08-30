59e5ba96ccb7543f1e9f780ed937f36059c8242d
Ajinkya Bodke <62661565+ajinkyabodke@users.noreply.github.com>
2020-08-30 12:28:21 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import EmojiObjectsIcon from "@material-ui/icons/EmojiObjects";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <EmojiObjectsIcon />
+        Keep Notes
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
